f0710ae2818edf6d1e3c1e6fb8789fbf419e0c19
Dhaval Potdar <dhavalpotdar@gmail.com>
2023-12-06 02:58:51 +0000

    addedexport SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=true to setup.sh

--- setup.sh
+++ setup.sh
@@ -2,10 +2,11 @@
 
 virtualenv ~/.venv
 source ~/.venv/bin/activate
-echo 'export SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=true' >> ~/.bashrc
-source ~/.bashrc
+export SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=true
 make install
 
 
 #append it to bash so every shell launches with it 
 echo 'source ~/.venv/bin/activate' >> ~/.bashrc
+echo 'export SKLEARN_ALLOW_DEPRECATED_SKLEARN_PACKAGE_INSTALL=True' >> ~/.bashrc
+
